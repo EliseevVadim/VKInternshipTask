5ca12f65b8fa477b23bb49027532a093884ae0a7
EliseevVadim <eliseevv02@mail.ru>
2023-05-08 11:00:02 +0300

    fix: add get users query validation

---
+++ VKInternshipTask.Application/Features/Users/Queries/GetUsers/GetUsersQueryValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VKInternshipTask.Application.Features.Users.Queries.GetUsers
+{
+    public class GetUsersQueryValidator : AbstractValidator<GetUsersQuery>
+    {
+        public GetUsersQueryValidator()
+        {
+            RuleFor(query => query.Page).GreaterThan(0);
+            RuleFor(query => query.Size).GreaterThan(0);
+        }
+    }
+}
